64a03346c5286796a0327369179e8e55d240e993
inspiretelapps <inspiretelapps@gmail.com>
2026-07-30 18:13:35 +0200

    feat: launch American Postcards travel journal

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ConvexReactClient } from "convex/react";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import App from "./App";
+import "./style.css";
+const client = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><ConvexAuthProvider client={client}><App /></ConvexAuthProvider></React.StrictMode>);
